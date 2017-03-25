746fd56bb3e1d082d2f9a961522857ba815eb5c9
Harshavardhan Vasam <ls_players@hotmail.com>
2017-03-25 14:18:56 -0400

    Initial Commit

---
+++ Pick Up Sports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pick Up Sports
+//
+//  Created by Harshavardhan Vasam on 2017-03-25.
+//  Copyright © 2017 Harshavardhan Vasam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
